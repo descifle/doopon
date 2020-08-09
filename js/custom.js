08d6137b8050eba5955fd8236668650729e81f7f
Descifle <descifle_itb@yahoo.com>
2020-08-09 16:26:37 -0400

    misc changes

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,29 @@
-$(document).ready(function () {
-    
 
+
+
+$(document).ready(function () {
+
+
+    //handle navbar
+
+    $(window).on('scroll', function() {
+        if($(window).scrollTop() < 20) {
+            $('#navbar').css({ 'height': ''}) 
+        } else {
+            $('#navbar').css({ 'height': '6rem'})
+        } 
+    })
+
+    // smooth scroll to specified anchor
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
+    //setup and initialize wow js 
     wow = new WOW({
         animateClass: 'animated',
         offset: 100
