2fb33a5bb8499e29e863b196dfbddac2d12afa83
Descifle <descifle_itb@yahoo.com>
2020-08-11 20:19:54 -0400

    added in gallery updated navigation updated misc

--- js/custom.js
+++ js/custom.js
@@ -3,14 +3,17 @@
 
 $(document).ready(function () {
 
+    const navTop = document.querySelector('.nav-top')
 
     //handle navbar
 
     $(window).on('scroll', function() {
         if($(window).scrollTop() < 20) {
-            $('#navbar').css({ 'height': ''}) 
+            $('#navbar').css({ 'height': ''})
+            
         } else {
             $('#navbar').css({ 'height': '6rem'})
+            $('#navbar').css({ 'opacity': '0.95'})
         } 
     })
 
@@ -23,6 +26,19 @@ $(document).ready(function () {
         }, 500);
     });
 
+    const scrollTo = () => {
+        $('html ,body').animate({scrollTop : 0}, 1200)
+    }
+
+    const scrollTopAppear = () => {
+        let y = window.scrollY
+        if (y >= 800) {
+            navTop.className = "nav-top show"
+        } else {
+            navTop.className = "nav-top hidden"
+        }
+    }
+
     //setup and initialize wow js 
     wow = new WOW({
         animateClass: 'animated',
@@ -30,5 +46,9 @@ $(document).ready(function () {
     });
     wow.init();
 
+    $('.nav-top').click(scrollTo)
+    $('.home').click(scrollTo)
     $('.slider').bxSlider();
+
+    window.addEventListener('scroll', scrollTopAppear)
 })
